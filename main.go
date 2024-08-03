64b3bea64766b9922aec7c47150624d6e4251b2e
Harmee Patel <daddy_davinci@Harmees-MBP.hitronhub.home>
2024-08-03 15:18:16 -0400

    feat: generating html from templates

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"harmeepatel.dev/web/pages"
+)
+
+func main() {
+
+	if err := os.MkdirAll("dist", os.ModePerm); err != nil {
+		log.Fatalf("failed to create output directory: %v", err)
+	}
+
+	f, err := os.Create("index.html")
+	if err != nil {
+		log.Fatalf("failed to create output file: %v", err)
+	}
+
+	err = pages.Index("harmeepatel").Render(context.Background(), f)
+	if err != nil {
+		log.Fatalf("failed to write output file: %v", err)
+	}
+}
