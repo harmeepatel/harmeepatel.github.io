576e851cdebc682e04c601aff4defadbc5eb51f4
Harmee Patel <daddy_davinci@Harmees-MBP.hitronhub.home>
2024-08-10 00:48:20 -0400

    fix: fixing mobile number

--- web/pages/index_templ.go
+++ web/pages/index_templ.go
@@ -100,7 +100,7 @@ func infoBar() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<a href=\"tel:+16718321245\" class=\"")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<a href=\"tel:+16478321245\" class=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
