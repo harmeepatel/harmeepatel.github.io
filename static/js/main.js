5ab20ff3c44930d61c2f669f8576dca7b147f61a
Harmee Patel <31391574+harmeepatel@users.noreply.github.com>
2024-10-29 00:18:52 -0400

    fix: minor bugs (#94)
    * feat: cleanup after generating html
    
    * fix: rebase issue
    
    * feat: removing raw css
    
    * feat: adding image viewing mode
    
    * fix: ignoring *_templ.go files
    
    * fix: ignoring main bin and adding towebp-l
    
    * fix: adding go.mod
    
    * fix: big screw up, merged main into dev and it messed up everyting
    
    * fix: workflow
    
    * fix: fixing .gitignore on main branch
    
    * fix: main.go
    
    * fix: removing .air.toml from main
    
    * fix: go.sum
    
    * fix: fixing wide image width in changing screen
    
    * fix: trying to fix nav on mobile
    
    * fix: fixing nav on mobile
    
    * fix: nav on mobile
    
    * fix: nav on mobile
    
    * fix: nav on mobile
    
    * fix: nav on mobile
    
    * fix: nav on mobile
    
    * fix: nav on mobile
    
    * feat: changing nav completely and accent color changes
    
    * fix: something went wrong generating css on previous commit
    
    * feat: fixing minor bugs
    
    ---------
    
    Co-authored-by: Harmee Patel <daddy_davinci@Harmees-MBP.hitronhub.home>

--- static/js/main.js
+++ static/js/main.js
@@ -1,26 +1,67 @@
 console.log(window.btoa("Can't take it when you die, but you can't live without it. ~J.Cole"))
 
-// on scroll nav shadow, blur and ring
-const offset = 16
+// document elements
+const nav = document.getElementById("nav")
+
+// remove hover underline in nav
+const resizeOffset = 768
+const navUl = nav.lastChild.lastChild
+const navA = navUl.getElementsByTagName("a")
+const hoverUnderline = Array.from(navUl.getElementsByTagName('span'))
+
+window.addEventListener("load", () => {
+    const pageName = window.location.pathname.split('/')
+    const navAArr = Array.from(navA)
+    const bold = "hover"
+    console.log(navAArr)
+    switch (pageName[pageName.length - 1].split('.')[0]) {
+        case "index":
+            navAArr[0].classList.add(bold);
+            break;
+        case "blog":
+            navAArr[1].classList.add(bold);
+            console.log("blog");
+            break;
+        case "photos":
+            navAArr[2].classList.add(bold);
+            console.log("photos");
+            break;
+    }
+
+    if (window.innerWidth < resizeOffset) {
+        hoverUnderline.forEach((e) => e.remove())
+    }
+});
+window.addEventListener("resize", () => {
+    if (window.innerWidth < resizeOffset) {
+        hoverUnderline.forEach((e) => e.remove())
+    }
+    if (window.innerWidth > resizeOffset) {
+        Array.from(navA).forEach((e, i) => e.appendChild(hoverUnderline[i]))
+    }
+});
+
+
+// on scroll add nav shadow, blur and ring
+const scrollOffset = 8
 const classes = ["ring-1", "dark:ring-white/60", "ring-black/50", "dark:shadow-[0_0_50px_-12px_rgba(0,0,0,1)]", "shadow-[0_0_50px_-12px_rgba(0,0,0,0.6)]", "backdrop-blur", "duration-200"]
 window.addEventListener("load", () => {
-    if (window.scrollY > offset) {
+    if (window.scrollY > scrollOffset) {
         for (let i = 0; i < classes.length; i++) {
             nav.classList.add(classes[i]);
         }
     }
 });
-window.onscroll = () => {
-    const nav = document.getElementById("nav")
 
-    if (window.scrollY > offset) {
+window.addEventListener("scroll", () => {
+    if (window.scrollY > scrollOffset) {
         for (let i = 0; i < classes.length; i++) {
             nav.classList.add(classes[i]);
         }
     }
-    if (window.scrollY < offset) {
+    if (window.scrollY < scrollOffset) {
         for (let i = 0; i < classes.length; i++) {
             nav.classList.remove(classes[i]);
         }
     }
-}
+})
