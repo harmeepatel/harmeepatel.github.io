c3aea02206e5f8d4cf914076c00aa438147c6527
Harmee Patel <harmeepatel@gmail.com>
2024-10-30 01:49:28 -0400

    fix: reverting to old design

--- static/js/photos.js
+++ static/js/photos.js
@@ -3,10 +3,6 @@ const closeDialogThreshold = 1024
 const modal = document.getElementById("image_modal")
 const modalImg = modal.querySelector("#modal_image")
 
-const photosMd = document.querySelector("#photos-md")
-const photosSm = document.querySelector("#photos-sm")
-const footer = document.querySelector("#footer")
-
 const imgGrid = document.getElementById("image-grid")
 const imgGridSrc = Array.from(imgGrid.getElementsByTagName("img"), (i) => i.src)
 const imgGridOrd = Array.from(imgGrid.getElementsByTagName("img"), (i) => i.id[0])
@@ -17,31 +13,7 @@ for (let i = 0; i < imgGridOrd.length; i++) {
     imgSrcArr[parseInt(ord)] = imgGridSrc[i]
 }
 
-const mdScreen = 768
-function imageView() {
-    if (window.innerWidth < mdScreen) {
-        document.body.replaceChild(photosSm, photosMd);
-        // document.body.classList.add("flex")
-        // document.body.classList.add("flex-col")
-        nav.classList.add("mx-auto")
-        nav.classList.add("my-0")
-    }
-    if (window.innerWidth >= mdScreen) {
-        document.body.replaceChild(photosMd, photosSm);
-        // document.body.classList.remove("flex")
-        // document.body.classList.remove("flex-col")
-        nav.classList.remove("mx-auto")
-        nav.classList.remove("my-0")
-    }
-}
-window.addEventListener("load", () => {
-    // change photo viewing mode on small screen
-    imageView()
-})
 window.addEventListener("resize", () => {
-    // change photo viewing mode on small screen
-    imageView()
-
     // remove modal if screen size less than 768px
     if (modal.open && window.innerWidth < closeDialogThreshold) {
         modal.close()
