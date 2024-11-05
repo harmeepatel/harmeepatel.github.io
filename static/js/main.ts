e6a08f0d47341d97f5ee6bcecb8e8afcd9db71f0
Harmee Patel <harmeepatel@gmail.com>
2024-11-05 11:11:26 -0500

    feat: moving images in image modal

--- static/js/main.ts
+++ static/js/main.ts
@@ -1,42 +1,47 @@
-"use strict";
-(function () {
-    console.log(window.btoa("Can't take it when you die, but you can't live without it. ~J.Cole"));
+(function() {
+    console.log(window.btoa("Can't take it when you die, but you can't live without it. ~J.Cole"))
+
     // document elements
-    const nav = document.getElementById("nav");
+    const nav: HTMLElement = document.getElementById("nav")!
+
     // --- remove hover underline in nav ---
-    const resizeOffset = 768;
-    const navLi = Array.from(nav.getElementsByTagName("li"));
-    const navA = navLi.map((elem) => elem.firstChild);
-    const underlineSpan = navLi.map((elem) => {
+    const resizeOffset: number = 768
+    const navLi = Array.from(nav.getElementsByTagName("li"))
+    const navA = navLi.map((elem) => elem.firstChild)
+    const underlineSpan = navLi.map((elem): ChildNode => {
         if (elem.lastChild == null) {
-            throw new Error("nav>ul>li>a not found");
+            throw new Error("nav>ul>li>a not found")
         }
         if (elem.lastChild.lastChild == null) {
-            throw new Error("nav>ul>li>a>span not found");
+            throw new Error("nav>ul>li>a>span not found")
         }
-        return elem.lastChild.lastChild;
-    });
+        return elem.lastChild.lastChild
+    })
+
     window.addEventListener("load", () => {
         if (window.innerWidth < resizeOffset) {
-            underlineSpan.forEach((span) => span.remove());
+            underlineSpan.forEach((span) => span.remove())
         }
     });
+
     window.addEventListener("resize", () => {
         if (window.innerWidth < resizeOffset) {
-            underlineSpan.forEach((span) => span.remove());
+            underlineSpan.forEach((span) => span.remove())
         }
         if (window.innerWidth > resizeOffset) {
             navA.forEach((a, i) => {
                 if (a == null) {
-                    throw new Error("nav>ul>li>a not found");
+                    throw new Error("nav>ul>li>a not found")
                 }
-                a.appendChild(underlineSpan[i]);
-            });
+                a.appendChild(underlineSpan[i])
+            })
         }
     });
+
+
     // --- on scroll add nav shadow, blur and ring ---
-    const scrollOffset = 8;
-    const classes = ["ring-1", "dark:ring-white/80", "ring-black/60", "dark:shadow-[0_0_50px_-12px_rgba(0,0,0,1)]", "shadow-[0_0_50px_-12px_rgba(0,0,0,0.6)]", "backdrop-blur", "duration-200"];
+    const scrollOffset = 8
+    const classes = ["ring-1", "dark:ring-white/80", "ring-black/60", "dark:shadow-[0_0_50px_-12px_rgba(0,0,0,1)]", "shadow-[0_0_50px_-12px_rgba(0,0,0,0.6)]", "backdrop-blur", "duration-200"]
     window.addEventListener("load", () => {
         if (window.scrollY > scrollOffset) {
             for (let i = 0; i < classes.length; i++) {
@@ -44,6 +49,7 @@
             }
         }
     });
+
     window.addEventListener("scroll", () => {
         if (window.scrollY > scrollOffset) {
             for (let i = 0; i < classes.length; i++) {
@@ -55,5 +61,5 @@
                 nav.classList.remove(classes[i]);
             }
         }
-    });
+    })
 })();
---
+++ static/js/photos.ts
@@ -0,0 +1,180 @@
+function addClasses(elem: HTMLElement, classes: string[]) {
+    for (const c of classes) {
+        elem.classList.add(c)
+    }
+}
+
+function removeClasses(elem: HTMLElement, classes: string[]) {
+    for (const c of classes) {
+        elem.classList.remove(c)
+    }
+}
+
+// open modal on clicking on an image if the screen is bigger than 1024px
+const modal = document.getElementById("image_modal")! as HTMLDialogElement
+const modalImg = modal.querySelector("#modal_image") as HTMLImageElement
+function openImageModal(e: HTMLImageElement) {
+    const closeDialogThreshold = 768
+    if (window.innerWidth >= closeDialogThreshold) {
+        modalImg.src = e.src
+        modal.showModal()
+    }
+    if (!modal.open) {
+        modalImg.src = ""
+    }
+}
+
+function imageSorter(a: Element, b: Element) {
+    const aNum = parseInt(a.id[0])
+    const bNum = parseInt(b.id[0])
+    if (aNum < bNum) {
+        return -1;
+    } else if (aNum > bNum) {
+        return 1;
+    }
+    return 0;
+}
+let imgCollection = document.getElementsByClassName("row")
+let imgArr = Array.from(imgCollection).sort(imageSorter)
+
+function leftImage(e: HTMLButtonElement) {
+    let prevImg: HTMLImageElement = imgArr[0] as HTMLImageElement;
+    let currImg = e.parentElement!.getElementsByTagName("img")[0]
+    let currImgId = -1;
+
+    imgArr.forEach((elem) => {
+        let img: HTMLImageElement = elem as HTMLImageElement
+        if (img.src === currImg.src) {
+            currImgId = parseInt(img.id[0])
+        }
+        if (currImgId === 0) {
+            prevImg = img
+        } else {
+            prevImg = imgArr[currImgId - 1] as HTMLImageElement
+        }
+    })
+
+    if (currImgId === 0) {
+        console.log("reached left edge")
+    }
+
+    modalImg.src = prevImg.src
+
+    if (!modal.open) {
+        modalImg.src = ""
+    }
+}
+
+function rightImage(e: HTMLButtonElement) {
+    let prevImg: HTMLImageElement = imgArr[0] as HTMLImageElement;
+    let currImg = e.parentElement!.getElementsByTagName("img")[0]
+    let currImgId = -1;
+
+    imgArr.forEach((elem) => {
+        let img: HTMLImageElement = elem as HTMLImageElement
+        if (img.src === currImg.src) {
+            currImgId = parseInt(img.id[0])
+        }
+        if (currImgId === imgArr.length - 1) {
+            prevImg = img
+        } else {
+            prevImg = imgArr[currImgId + 1] as HTMLImageElement
+        }
+    })
+
+    if (currImgId === imgArr.length - 1) {
+        console.log("reached right edge")
+    }
+
+    modalImg.src = prevImg.src
+
+    if (!modal.open) {
+        modalImg.src = ""
+    }
+}
+
+
+// --- main ---
+(function() {// -- utils --
+    // setting image order
+    const imgGrid = document.getElementById("image-grid")!
+    const imgGridSrc = Array.from(imgGrid.getElementsByTagName("img"), (i) => i.src)
+    const imgGridOrd = Array.from(imgGrid.getElementsByTagName("img"), (i) => i.id[0])
+    const imgSrcArr = new Array(imgGridOrd.length)
+    for (let i = 0; i < imgGridOrd.length; i++) {
+        const ord = imgGridOrd[i][0]
+        imgSrcArr[parseInt(ord)] = imgGridSrc[i]
+    }
+
+    // replacing main sections for small and greater screen sizes
+    const photosMd = document.querySelector("#photos-md")!
+    const photosSm = document.querySelector("#photos-sm")!
+    const parentNd = photosMd.parentNode || photosSm.parentNode
+    const mdScreen = 768
+    function imageView() {
+        // const classes = ["mx-auto", "my-0", "mt-4"]
+        if (photosSm === null || photosMd === null || parentNd === null) {
+            throw new Error("photosSm, photosMd, or parent node to photos-md in main not found")
+        }
+        if (window.innerWidth < mdScreen) {
+            parentNd.replaceChild(photosSm, photosMd)
+        }
+        if (window.innerWidth >= mdScreen) {
+            parentNd.replaceChild(photosMd, photosSm)
+        }
+    }
+
+    // setting padding and aspect-ratio based on image orientation
+    const imgWraper = document.querySelectorAll(".img-wraper")
+    window.addEventListener("load", () => {
+        // change photo viewing mode on small screen
+        imageView()
+
+        imgWraper[0].classList.add("mt-28")
+
+        // different margin for 5x4 images
+        imgWraper.forEach((elem) => {
+            const image = elem.lastChild! as HTMLImageElement
+            let width = image.width
+            let height = image.height
+
+            if (width > height) {
+                elem.classList.add("aspect-[5/4]")
+            } else {
+                elem.classList.add("aspect-[4/5]")
+            }
+        })
+    })
+
+    // --- adding nav styles on scroll ---
+    const nav: HTMLElement = document.getElementById("nav")!
+    photosSm.addEventListener("scroll", () => {
+        const scrollOffset = 8
+        const classes = ["ring-1", "dark:ring-white/80", "ring-black/60", "dark:shadow-[0_0_50px_-12px_rgba(0,0,0,1)]", "shadow-[0_0_50px_-12px_rgba(0,0,0,0.6)]", "backdrop-blur", "duration-200"]
+        if (photosSm.scrollTop > scrollOffset) {
+            for (let i = 0; i < classes.length; i++) {
+                nav.classList.add(classes[i]);
+            }
+        }
+        if (photosSm.scrollTop < scrollOffset) {
+            for (let i = 0; i < classes.length; i++) {
+                nav.classList.remove(classes[i]);
+            }
+        }
+    })
+
+    // --- modal ---
+    const closeDialogThreshold = 768
+    const modal = document.getElementById("image_modal")! as HTMLDialogElement
+    window.addEventListener("resize", () => {
+        // change photo viewing mode on small screen
+        imageView()
+
+        // remove modal if screen size less than 768px
+        if (modal.open && window.innerWidth < closeDialogThreshold) {
+            modal.close()
+        }
+    })
+
+
+})();
